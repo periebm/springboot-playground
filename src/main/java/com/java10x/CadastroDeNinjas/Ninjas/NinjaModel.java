ebf3eb76a76a071cbc9abae90206f9b303d11ceb
periebm <periclesebmendes@gmail.com>
2025-01-21 20:35:23 -0300

    feat: adicionado models Ninjas e Missoes

---
+++ src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -0,0 +1,31 @@
+package com.java10x.CadastroDeNinjas.Ninjas;
+
+import com.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+    private int idade;
+
+    // @ManyToOne um ninja tem uma unica missão
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") //Foreig Key
+    private MissoesModel missoes;
+}
